49579d952ba5825e6ee8c5dd4774fcfc9454e21b
Thoan <pdthoan.20it11@vku.udn.vn>
2022-09-20 00:17:40 +0700

    complete layout Create a New Appropval Matrix

---
+++ app/src/main/java/com/example/uienouvo/add_approval_matrix.kt
@@ -0,0 +1,11 @@
+package com.example.uienouvo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_approval_matrix : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_approval_matrix)
+    }
+}
